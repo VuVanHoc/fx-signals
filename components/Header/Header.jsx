4134b2c732c4df3354307f9b0173f7ce25165f5e
HocVuVan <van.hocvu@mission.plus>
2021-07-17 22:44:08 +0700

    fix escape character

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -13,9 +13,9 @@ function Header() {
         <h1 className={styles.h1}>Forex Signals</h1>
         <p className={styles.headerContent}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled
         </p>
       </div>
     </div>
