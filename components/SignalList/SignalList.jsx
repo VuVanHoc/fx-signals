5773699a6ad46087d166537bd005e1cd7c7e63ef
HocVuVan <van.hocvu@mission.plus>
2021-07-22 00:30:46 +0700

    fix UI + get list tabs from api

--- components/SignalList/SignalList.jsx
+++ components/SignalList/SignalList.jsx
@@ -6,53 +6,29 @@ import SignalItem from "./SignalItem";
 import api from "../../api";
 
 export default function SignalList() {
-  const tabs = useMemo(
-    () => [
-      { title: "BTC H1", value: 0 },
-      { title: "BTC H4", value: 1 },
-      { title: "BTC D", value: 2 },
-      { title: "GOLD H1", value: 3 },
-      // { title: "GOLD H4", value: 4 },
-      { title: "GOLD D", value: 5 },
-    ],
-    []
-  );
-  const [tabIndex, setTabIndex] = useState(0);
+  // const tabs = useMemo(
+  //   () => [
+  //     { title: "BTC H1", value: 0 },
+  //     { title: "BTC H4", value: 1 },
+  //     { title: "BTC D", value: 2 },
+  //     { title: "GOLD H1", value: 3 },
+  //     // { title: "GOLD H4", value: 4 },
+  //     { title: "GOLD D", value: 5 },
+  //   ],
+  //   []
+  // );
+  const [tabIndex, setTabIndex] = useState(""); // This is display name of tab.
   const [signals, setSignal] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [tabs, setTabs] = useState([]);
 
-  const fetchSignals = useCallback(async (tabIndex) => {
+  const fetchSignals = useCallback(async (tab) => {
+    if (!tab) return;
     setLoading(true);
     try {
-      let dataType = "BTCUSDT";
-      let periodType = "H1";
+      let dataType = tab?.type;
+      let periodType = tab?.periodType;
 
-      switch (tabIndex) {
-        case 0:
-          dataType = "BTCUSDT";
-          periodType = "H1";
-          break;
-        case 1:
-          dataType = "BTCUSDT";
-          periodType = "H4";
-          break;
-        case 2:
-          dataType = "BTCUSDT";
-          periodType = "D";
-          break;
-        case 3:
-          dataType = "XAUUSD";
-          periodType = "H1";
-          break;
-        case 4:
-          dataType = "XAUUSD";
-          periodType = "H4";
-          break;
-        case 5:
-          dataType = "XAUUSD";
-          periodType = "D";
-          break;
-      }
       const params = {
         data: dataType,
         period: periodType,
@@ -72,9 +48,9 @@ export default function SignalList() {
     }
   }, []);
   const handleClickTab = useCallback(
-    (value) => {
-      setTabIndex(value);
-      fetchSignals(value);
+    (tab) => {
+      setTabIndex(tab);
+      fetchSignals(tab);
     },
     [setTabIndex, fetchSignals]
   );
@@ -105,9 +81,22 @@ export default function SignalList() {
         ?.reduce((acc, signal) => acc + signal.profit, 0) * -1,
     [signals]
   );
+  const fetchListLabel = useCallback(async () => {
+    try {
+      const res = await api.get(`/labels`);
+      if (res) {
+        setTabs(res.data);
+        handleClickTab(res.data?.[0]);
+      }
+    } catch (error) {}
+  }, [setTabs, handleClickTab]);
   useEffect(() => {
-    fetchSignals(0);
-  }, [fetchSignals]);
+    fetchListLabel();
+  }, [fetchListLabel]);
+
+  // useEffect(() => {
+  // fetchSignals(0);
+  // }, [fetchSignals]);
   return (
     <div className={styles.container}>
       {/* Title */}
@@ -123,14 +112,14 @@ export default function SignalList() {
             {tabs.map((tab, index) => {
               return (
                 <div
-                  onClick={() => handleClickTab(tab.value)}
+                  onClick={() => handleClickTab(tab)}
                   key={index}
                   className={cx({
                     [styles.tabItem]: true,
-                    [styles.tabItemActive]: tab.value === tabIndex,
+                    [styles.tabItemActive]: tab.name === tabIndex?.name,
                   })}
                 >
-                  {tab.title}
+                  {tab.name}
                 </div>
               );
             })}
@@ -145,12 +134,25 @@ export default function SignalList() {
           <p
             className={cx(styles.numberSummary, styles.redColor)}
           >{`Lost = ${totalSignalLost}`}</p>
+          <p className={cx(styles.numberSummary, styles.greenColor)}>
+            <span>{`     |     `}</span>
+            {`Win rate = ${
+              totalSignalWon + totalSignalLost !== 0
+                ? Math.round(
+                    (totalSignalWon * 100) / (totalSignalWon + totalSignalLost)
+                  )
+                : 0
+            }%`}
+          </p>
           <p
             className={cx(styles.numberSummary, {
               [styles.greenColor]: totalWon - totalLost > 0,
               [styles.redColor]: totalWon - totalLost < 0,
             })}
-          >{` | Profit = ${Number(totalWon - totalLost).toFixed(2)}`}</p>
+          >
+            <span className={styles.hiddenOnMobile}>{`     |     `}</span>
+            {`Profit = ${Number(totalWon - totalLost).toFixed(2)}`}
+          </p>
         </div>
         <div className={styles.contentTab}>
           {loading ? (
